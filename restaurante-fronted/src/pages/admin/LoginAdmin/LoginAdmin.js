6b0904e10a66af056044fc3899c44dc9a38d71b3
carlosdiazz <c.diazadriann@gmail.com>
2022-10-19 18:25:18 -0400

    Login

--- restaurante-fronted/src/pages/admin/LoginAdmin/LoginAdmin.js
+++ restaurante-fronted/src/pages/admin/LoginAdmin/LoginAdmin.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import {LoginForm} from '../../../components/Admin'
-import './LoginAdmin.scss'
+import React from 'react';
+import { LoginForm } from "../../../components/Admin";
+import "./LoginAdmin.scss";
+import logo1 from './logo.png';
+import logo2 from "./rest.jpg"
 
 export function LoginAdmin() {
   return (
-    <div className='login-admin'>
-        <div className='login-admin__content'>
-          <h1>Entrar al panel</h1>
-          <LoginForm />
+    <div className='login-admin context' >
+
+      <div className="imagen2">
+            <img src={logo2} className="imagen2" alt=''/>
         </div>
+        <div className='login-admin__content'>
+        <div className="imagen1">
+            <img src={logo1} className="imagen1" alt=''/>
+        </div>
+          <LoginForm />
     </div>
-  )
+    </div>
+  );
 }
