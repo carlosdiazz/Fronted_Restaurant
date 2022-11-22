f1864ddf044ef3e7c7b7044d7a242d66ff65f31d
Carlos <c.diazadriann@gmail.com>
2022-11-21 23:19:08 -0400

    Pa la nube

--- restaurante-fronted/src/config/config.js
+++ restaurante-fronted/src/config/config.js
@@ -1,7 +1,7 @@
 //import * as dotenv from 'dotenv';
 //dotenv.config();
 
-export const BASE_API_URL = "http://localhost:4500/api/v1"
+export const BASE_API_URL = "https://restaurantebackend-production.up.railway.app/api/v1"
 //export const BASE_API_URL = "http://172.24.92.116:4500/api/v1"
 
 export const TOKEN = "token";
